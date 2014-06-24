d484d8533dd79082c33fc30788403e4390b499d3
Laurent HUET <laurent.huet@gmail.com>
2014-06-24 09:22:34 +0200

    simplification des lambdas

--- src/main/java/fr/lhuet/teleinfo/WebVerticle.java
+++ src/main/java/fr/lhuet/teleinfo/WebVerticle.java
@@ -19,46 +19,39 @@ public class WebVerticle extends WebServerBase {
 
         RouteMatcher matcher = new RouteMatcher();
 
-        matcher.get("/isAlive", (HttpServerRequest req) -> {
-            req.response().end("Teleinfo Web Application started !");
-        });
+        return matcher
+            .get("/isAlive", req -> req.response().end("Teleinfo Web Application started !"))
 
-        matcher.get("/rest/inst/p", (HttpServerRequest req) -> {
-            eb.send("teleinfo-data", "get", (Message<JsonObject> teleinfo) -> {
-                String pinst = teleinfo.body().getInteger("PAPP").toString();
-                req.response().end(pinst);
-            });
-        });
+            .get("/rest/inst/p", req -> eb.send("teleinfo-data", "get",
+                (Message<JsonObject> teleinfo) -> {
+                    String pinst = teleinfo.body().getInteger("PAPP").toString();
+                    req.response().end(pinst);
+                }))
 
-        matcher.get("/rest/inst/i", (HttpServerRequest req) -> {
-            eb.send("teleinfo-data", "get", (Message<JsonObject> teleinfo) -> {
+            .get("/rest/inst/i", req -> eb.send("teleinfo-data", "get",
+                (Message<JsonObject> teleinfo) -> {
                     String iinst = teleinfo.body().getInteger("IINST").toString();
                     req.response().end(iinst);
-            });
-        });
+                }))
 
-        matcher.get("/rest/inst/index", (HttpServerRequest req) -> {
-            eb.send("teleinfo-data", "get", (Message<JsonObject> teleinfo) -> {
+            .get("/rest/inst/index", req -> eb.send("teleinfo-data", "get",
+                (Message<JsonObject> teleinfo) -> {
                     String index = teleinfo.body().getInteger("INDEX").toString();
                     req.response().end(index);
-            });
-        });
+                }))
 
-        matcher.get("/rest/dhw/dhw", (HttpServerRequest req) -> {
-            eb.send("dhw-temp", "dhw", (Message<String> msg) -> {
+            .get("/rest/dhw/dhw", req -> eb.send("dhw-temp", "dhw",
+                (Message<String> msg) -> {
                     req.response().end(String.valueOf(msg.body()));
-            });
-        });
+                }))
 
-        matcher.get("/rest/dhw/buffer", (HttpServerRequest req) -> {
-            eb.send("dhw-temp", "buffer", (Message<String> msg) -> {
+            .get("/rest/dhw/buffer", req -> eb.send("dhw-temp", "buffer",
+                (Message<String> msg) -> {
                     req.response().end(String.valueOf(msg.body()));
-            });
-        });
+                }))
 
-        matcher.noMatch(staticHandler());
+            .noMatch(staticHandler());
 
-        return matcher;
     }
 
 }
